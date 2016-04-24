b7b2d01f67ec442c5b419542900405c82f22900c
gendainote2000 <suimin2000@outlook.jp>
2016-04-24 16:33:16 +0900

    browser

--- src/net/mitl_gr/minomb_excavator/Main.java
+++ src/net/mitl_gr/minomb_excavator/Main.java
@@ -1,7 +1,29 @@
 package net.mitl_gr.minomb_excavator;
 
-public class Main {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.scene.web.WebView;
+import javafx.stage.Stage;
+
+public class Main extends Application {
 	public static void main(String[] args){
-		System.out.print("Minomb Excavator");
+		Application.launch(Main.class, args);
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		primaryStage.setTitle("Minomb Excavator");
+
+		WebView webview = new WebView();
+		webview.getEngine().load("http://google.co.jp/");
+
+		StackPane root = new StackPane();
+		root.getChildren().add(webview);
+
+		Scene scene = new Scene(root, 500, 250);
+
+	    primaryStage.setScene(scene);
+		primaryStage.show();
 	}
 }
